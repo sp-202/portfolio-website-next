e3338fc9798803c54acfde799fb12e678079cca9
subhodeep <subhodeeppal64@gmail.com>
2025-05-04 19:35:25 +0530

    min-h-screen removed and FaChevronUp icon added

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Skills from "@/components/Skills";
+import { FaChevronUp } from "react-icons/fa";
 
 export default function Page() {
   const [showTopBtn, setShowTopBtn] = useState(false);
@@ -23,19 +24,29 @@ export default function Page() {
   return (
     <main className="font-sans bg-white text-black dark:bg-gray-900 dark:text-white">
       <Navbar />
-      <section id="home" className="min-h-screen pt-24 max-w-screen-xl mx-auto"><HomeSection /></section> {/* Added padding-top to avoid overlap */}
-      <section id="about" className="min-h-screen pt-20"><AboutSection /></section>
-      <section id="skills" className="min-h-screen pt-20"><Skills /></section>
-      <section id="projects" className="min-h-screen pt-20"><ProjectsSection /></section>
-      <section id="contact" className="min-h-screen pt-20"><ContactSection /></section>
+      <section id="home" className="pt-24 max-w-screen-xl mx-auto">
+        <HomeSection />
+      </section>
+      <section id="about" className="py-20 max-w-screen-xl mx-auto px-10">
+        <AboutSection />
+      </section>
+      <section id="skills" className="py-20 max-w-screen-xl mx-auto px-12">
+        <Skills />
+      </section>
+      <section id="projects" className="py-20 max-w-screen-xl mx-auto">
+        <ProjectsSection />
+      </section>
+      <section id="contact" className="min-h-screen py-20 max-w-screen-xl mx-auto">
+        <ContactSection />
+      </section>
       <Footer />
       {showTopBtn && (
         <Link
-          href="#home"
-          className="fixed bottom-6 right-6 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition dark:bg-blue-400 dark:hover:bg-blue-500"
-        >
-          ↑
-        </Link>
+        href="#home"
+        className="fixed bottom-6 p-3 hover:p-4 right-6 bg-blue-500 text-white hover:text-blue-500 rounded-full z-10 shadow-lg hover:shadow-xl hover:bg-blue-50 transition-all duration-300"
+      >
+        <FaChevronUp className="text-2xl" />
+      </Link>
       )}
     </main>
   );
